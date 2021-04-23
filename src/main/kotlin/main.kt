ebd01caca56a0ec459784ae23ffb70da9f20957d
Park-12 <sohyunp96@gmail.com>
2021-04-23 17:33:34 +0900

    게시물 작성 날짜 저장

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,6 @@
+import java.text.SimpleDateFormat
+
+
 fun readLineTrim() = readLine()!!.trim()
 
 fun main() {
@@ -19,11 +22,14 @@ fun main() {
             }
             "article write" -> {
                 val id = articlesLastId + 1
+                val regDate = Util.getNowDateStr()
+                val updateDate = Util.getNowDateStr()
+
                 print("제목 : ")
                 val title = readLineTrim()
                 print("내용 : ")
                 val body = readLineTrim()
-                val article = Article(id, title, body)
+                val article = Article(id, regDate, updateDate, title, body)
 
                 println("${id}번 게시물이 작성되었습니다.")
 
@@ -32,10 +38,10 @@ fun main() {
                 articlesLastId = id
             }
             "article list" -> {
-                println("번호 / 제목")
+                println("번호 / 작성날짜 / 제목")
 
                 for ( article in articles ) {
-                    println("${article.id} / ${article.title}")
+                    println("${article.id} / ${article.regDate} / ${article.title}")
                 }
             }
             else -> {
@@ -49,6 +55,16 @@ fun main() {
 
 data class Article(
     val id: Int,
+    val regDate: String,
+    val updateDate: String,
     val title: String,
     val body: String
 )
+
+object Util {
+    fun getNowDateStr(): String {
+        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
+        return dateFormat.format(System.currentTimeMillis())
+    }
+}
